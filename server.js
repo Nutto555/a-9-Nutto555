ce1bc05b5176d2c48c6fb583aea32868655dd4d3
NUT <89127454+Nutto555@users.noreply.github.com>
2024-03-28 22:21:34 +0700

    a-9

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+// Route files
+const hospitals = require('./routes/hospitals');
+const appointments = require('./routes/appointments');
+const auth = require('./routes/auth');
+
+
+// Load env vars
+dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
+
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+//Mount routers
+app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth',auth);
+app.use('/api/v1/appointments',appointments); 
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, console.log("Server running in", process.env.NODE_ENV, 'mode on port', PORT));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
